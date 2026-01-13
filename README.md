b5855d42012c28c6367b0f3b72e25dcf6e409184
Owen Mundy <omundy@gmail.com>
2026-01-13 09:50:07 -0500

    Adding content

--- README.md
+++ README.md
@@ -7,13 +7,3 @@
 Tutorials, demos, and resources for DIG 345 Radical Software
 
 
-## Tutorials
-
-- https://github.com/omundy/learn-javascript
-- https://github.com/omundy/sample-node-tools
-
-
-
-- Thinking Through Data [Youtube Playlist](https://www.youtube.com/playlist?list=PLhpnnpt3tw-SGxCwGuTLMgKpf4I74Ftdn)
-
-
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 /**
- * Config file for Astro/Starlight SSG. Other examples:
- * Astro/Starlight https://github.com/criticalwebdesign/wiki
- * Astro/Starlight https://github.com/omundy/game-dev-book
- * Astro https://github.com/omundy/camplajolla
+ * Config file for Astro/Starlight SSG
  */
 
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import mdx from '@astrojs/mdx'
 // import relativeLinks from 'astro-relative-links'
 import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +17,9 @@ export default defineConfig({
 		// relativeLinks(),
 		starlight({
 			title: 'DIG 345 Radical Software',
+			logo: {
+				src: './public/images/simple-net-art-diagram/simple-net-art-diagram-150w.png',
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/omundy/dig345-radical-software' }],
 			editLink: {
 				baseUrl: 'https://omundy.github.io/dig345-radical-software',
@@ -29,33 +29,34 @@ export default defineConfig({
 				{
 					label: 'Schedule',
 					items: [ // Each item here is one entry in the navigation menu.
-						{ label: 'Week 1 - Node.js, Jamstack', link: 'schedule#week-1' },
-						{ label: 'Week 2 - Express APIs', link: 'schedule#week-2' },
-						{ label: 'Week 3 - Sound & Music', link: 'schedule#week-3' },
-						{ label: 'Week 4 - Reactivity', link: 'schedule#week-4' },
-						{ label: 'Week 5 - Three.js', link: 'schedule#week-5' },
-						{ label: 'Week 6 - Three.js', link: 'schedule#week-6' },
-						{ label: 'Week 7 - Final Proposals', link: 'schedule#week-7' },
-						{ label: 'Weeks 8-15 - Final Project', link: 'schedule#week-8' },
+						{ label: 'Course Overview', link: 'schedule' },
+						{ label: 'Week 1 - Node.js, Jamstack', link: 'schedule#1-1-nodejs' },
+						{ label: 'Week 2 - Express APIs', link: 'schedule#2-1-nodeexpress' },
+						{ label: 'Week 3 - Databases', link: 'schedule#3-1-databases' },
+						{ label: 'Week 4 - Reactivity', link: 'schedule#4-1-reactivity' },
+						{ label: 'Week 5 - Sound & Music', link: 'schedule#5-1' },
+						{ label: 'Week 6 - Three.js', link: 'schedule#6-1-threejs' },
+						{ label: 'Week 7 - Final Proposals', link: 'schedule#7-1' },
+						{ label: 'Week 8-15 - Final Project', link: 'schedule#8-1' },
 					],
 				},
-				{ label: 'Reference', autogenerate: { directory: 'reference' }, },
+				// { label: 'Reference', autogenerate: { directory: 'reference' }, },
 				{ label: 'Tutorials', autogenerate: { directory: 'tutorials' }, }, // autogenerate nav from files in folder
 			],
 			customCss: [
 				'./src/styles/custom.css', // relative path to custom CSS file
-				'./src/styles/global.css', // tailwind CSS 
+				'./src/styles/tailwind-overrides.css', // fixes to tailwind CSS 
 			],
-            components: {
-                // Override the default components
-                SiteTitle: './src/components/SiteTitle.astro',
-            },
+			components: {
+				// Override the default components
+				SiteTitle: './src/components/SiteTitle.astro',
+			},
 		}),
 	],
 	vite: {
 		plugins: [
 			// @ts-ignore or as any
-			tailwindcss(),
+			tailwindcss()
 		]
 	},
 });
