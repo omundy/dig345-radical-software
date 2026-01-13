c3f35f510a27d2c7ba257cf362a261372ec77cbc
Owen Mundy <omundy@gmail.com>
2026-01-13 12:29:11 -0500

    Finish syllabus

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-![simple net art diagram](public/images/simple-net-art-diagram/simple-net-art-diagram-150w.png)
+<!-- ![simple net art diagram](public/images/simple-net-art-diagram/simple-net-art-diagram-150w.png) -->
 
 
 # DIG 345 - Radical Software
@@ -7,3 +7,4 @@
 Tutorials, demos, and resources for DIG 345 Radical Software
 
 
+https://omundy.github.io/dig345-radical-software/
--- astro.config.mjs
+++ astro.config.mjs
@@ -25,22 +25,26 @@ export default defineConfig({
 				baseUrl: 'https://omundy.github.io/dig345-radical-software',
 			},
 			sidebar: [
-				{ label: 'Home', link: './' },
-				{
-					label: 'Schedule',
-					items: [ // Each item here is one entry in the navigation menu.
-						{ label: 'Course Overview', link: 'schedule' },
-						{ label: 'Week 1 - Node.js, Jamstack', link: 'schedule#1-1-nodejs' },
-						{ label: 'Week 2 - Express APIs', link: 'schedule#2-1-nodeexpress' },
-						{ label: 'Week 3 - Databases', link: 'schedule#3-1-databases' },
-						{ label: 'Week 4 - Reactivity', link: 'schedule#4-1-reactivity' },
-						{ label: 'Week 5 - Sound & Music', link: 'schedule#5-1' },
-						{ label: 'Week 6 - Three.js', link: 'schedule#6-1-threejs' },
-						{ label: 'Week 7 - Final Proposals', link: 'schedule#7-1' },
-						{ label: 'Week 8-15 - Final Project', link: 'schedule#8-1' },
-					],
-				},
+				{ label: 'Schedule', link: './' },
+				{ label: 'Syllabus', link: './syllabus' },
+				{ label: 'Assignments', link: './assignments' },
+
+				// {
+				// 	label: 'Schedule',
+				// 	items: [ // Each item here is one entry in the navigation menu.
+				// 		{ label: 'Course Overview', link: 'schedule' },
+				// 		{ label: 'Week 1 - Node.js, Jamstack', link: 'schedule#1-1-nodejs' },
+				// 		{ label: 'Week 2 - Express APIs', link: 'schedule#2-1-astrojs' },
+				// 		{ label: 'Week 3 - Databases', link: 'schedule#3-1-databases' },
+				// 		{ label: 'Week 4 - Reactivity', link: 'schedule#4-1-reactivity' },
+				// 		{ label: 'Week 5 - Sound & Music', link: 'schedule#5-1' },
+				// 		{ label: 'Week 6 - Three.js', link: 'schedule#6-1-threejs' },
+				// 		{ label: 'Week 7 - Final Proposals', link: 'schedule#7-1' },
+				// 		{ label: 'Week 8-15 - Final Project', link: 'schedule#8-1' },
+				// 	],
+				// },
 				// { label: 'Reference', autogenerate: { directory: 'reference' }, },
+				
 				{ label: 'Tutorials', autogenerate: { directory: 'tutorials' }, }, // autogenerate nav from files in folder
 			],
 			customCss: [
